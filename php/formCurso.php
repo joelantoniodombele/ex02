bc1363aff4288fcc8bd9f9edd3251c506c443a1c
joelantoniodombele <150440673+joelantoniodombele@users.noreply.github.com>
2023-11-13 15:38:35 +0100

    Update formCurso.php

---
+++ php/formCurso.php
@@ -0,0 +1,9 @@
+<form method="post">
+    <label for="">Nome do curso</label>
+    <input type="text"><br><br>
+
+    <label for="">Categoria</label>
+    <select name="" id="">
+        
+    </select>
+</form>
