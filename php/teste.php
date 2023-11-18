1ae247684806986c6b6a313aa07ff9172bc8212b
joelantoniodombele <150440673+joelantoniodombele@users.noreply.github.com>
2023-11-18 07:55:21 +0100

    exibir imagem

--- php/teste.php
+++ php/teste.php
@@ -1,75 +1,52 @@
 <?php
 require_once 'connection.php';
 
+function uploadImagem($arquivo, $diretorioDestino) {
+    // ... código da função uploadImagem ...
+
+    // Obtém o nome do arquivo enviado
+    $nomeArquivo = $arquivo['name'];
+
+    // Define o caminho completo da imagem após o upload
+    $caminho_completo = 'uploads/imagens/cursos/' . $nomeArquivo;
+
+    return $caminho_completo;
+}
+
+
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $database = new DB();
     $conn = $database->connect();
 
     // Valida os dados do formulário
+
     $nome_curso = trim($_POST["nome_curso"]);
     $categoria_id = $_POST["categoria"];
     $instrutor_id = $_POST["instrutor"];
     $descricao = trim($_POST["descricao"]);
     $imagem = $_FILES["imagem"];
 
+    // ... (código de validação dos campos do formulário) ...
+
     if (empty($nome_curso) || $categoria_id === "" || $instrutor_id === "" || $imagem["error"] !== UPLOAD_ERR_OK || $imagem["size"] > 1000000) {
         echo "Erro nos dados do formulário.";
         exit;
     }
+    // Executa a função de upload da imagem
+    $arquivo = $_FILES['imagem'];
+    $diretorioDestino = 'uploads/imagens/cursos'; // Substitua pelo seu diretório real
 
-    $pasta = "uploads/imagens/cursos";
-    $caminho_completo = $pasta . "/" . basename($imagem["name"]);
+    $resultadoUpload = uploadImagem($arquivo, $diretorioDestino);
 
-    // Verifica o tipo de arquivo antes de mover para o destino
-    $extensoes_permitidas = array('jpg', 'jpeg', 'png', 'gif');
-    $extensao_arquivo = strtolower(pathinfo($caminho_completo, PATHINFO_EXTENSION));
-
-    if (!in_array($extensao_arquivo, $extensoes_permitidas)) {
-        echo "Tipo de arquivo não permitido.";
+    // Verifica o resultado do upload da imagem
+    if (strpos('$resultadoUpload', 'Erro') !== false) {
+        echo $resultadoUpload;
         exit;
-    }
-
-    // Verifica se o arquivo já existe antes de mover
-    if (file_exists($caminho_completo)) {
-        echo "O arquivo já existe.";
-        exit;
-    }
-
-    if (!move_uploaded_file($imagem["tmp_name"], $caminho_completo)) {
-      If (!move_uploaded_file($imagem["tmp_name"], $caminho_completo)) {
-        switch ($imagem['error']) {
-            case UPLOAD_ERR_INI_SIZE:
-                echo "O arquivo excede o limite definido na configuração do PHP (upload_max_filesize).";
-                break;
-            case UPLOAD_ERR_FORM_SIZE:
-                echo "O arquivo enviado excede o limite definido no formulário HTML.";
-                break;
-            case UPLOAD_ERR_PARTIAL:
-                echo "O upload do arquivo foi feito parcialmente.";
-                break;
-            case UPLOAD_ERR_NO_FILE:
-                echo "Nenhum arquivo foi enviado.";
-                break;
-            case UPLOAD_ERR_NO_TMP_DIR:
-                echo "Pasta temporária ausente.";
-                break;
-        case UPLOAD_ERR_CANT_WRITE:
-            echo "Falha ao escrever o arquivo no disco.";
-            break;
-        case UPLOAD_ERR_EXTENSION:
-            echo "Uma extensão do PHP interrompeu o upload.";
-            break;
-        default:
-            echo "Ocorreu um erro desconhecido ao enviar o arquivo de imagem.";
-            break;
-    }
-    exit;
-  
-  
-  }
     }
 
     // Insere o caminho do arquivo de imagem no banco de dados
+    $caminho_completo = $resultadoUpload; // Defina o caminho completo da imagem após o upload
+
     $stmt = $conn->prepare("INSERT INTO cursos (nome, categoria_id, instrutor_id, descricao, imagem) VALUES (:nome, :categoria_id, :instrutor_id, :descricao, :imagem)");
 
     $stmt->bindParam(':nome', $nome_curso);
@@ -86,3 +63,20 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     }
 }
 ?>
+<?php
+
+    // ... (seu código de inserção no banco de dados)
+if ($stmt->execute()) {
+    // Obtenha o ID do registro inserido
+    $ultimo_id = $conn->lastInsertId();
+    // Redirecione para a página de exibição da imagem com o ID do registro
+    header("Location: exibe_imagem.php?id=$ultimo_id");
+    exit;
+}
+
+
+?>
+
+
+
+
